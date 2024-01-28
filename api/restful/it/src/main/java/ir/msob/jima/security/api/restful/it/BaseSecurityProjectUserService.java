8e0b7b3e102e4ee71895eb6aff4d5ffcafde0e29
yaghob abdi <y.abdi@rashasoft.ir>
2024-01-28 12:52:21 +0330

    Add rest it module

---
+++ api/restful/it/src/main/java/ir/msob/jima/security/api/restful/it/BaseSecurityProjectUserService.java
@@ -0,0 +1,95 @@
+package ir.msob.jima.security.api.restful.it;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ir.msob.jima.core.beans.configuration.JimaConfigProperties;
+import ir.msob.jima.core.commons.security.BaseUser;
+import ir.msob.jima.core.commons.security.BaseUserService;
+import ir.msob.jima.core.commons.security.ClaimKey;
+import ir.msob.jima.core.commons.security.UserInfoUtil;
+import lombok.SneakyThrows;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.Map;
+import java.util.Optional;
+
+
+public interface BaseSecurityProjectUserService extends BaseUserService {
+    JimaConfigProperties getJimaConfigProperties();
+
+    ObjectMapper getObjectMapper();
+
+
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>, A extends Authentication> Optional<USER> getUser(A authentication) {
+        if (authentication != null) {
+            JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
+            return getUser(jwtAuthenticationToken.getTokenAttributes());
+        }
+        return Optional.empty();
+    }
+
+
+    @SneakyThrows
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>> Optional<USER> getUser(String userInfo, Class<USER> userClass) {
+        return UserInfoUtil.decodeUser(getObjectMapper(), userInfo, userClass);
+    }
+
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>, P extends Principal> Optional<USER> getUser(P principal) {
+        if (principal != null) {
+            JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) principal;
+            Jwt jwt = jwtAuthenticationToken.getToken();
+
+            return getUser(jwt.getClaims());
+        }
+        return Optional.empty();
+    }
+
+
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>> Optional<USER> getUser(String userInfo, Map<String, Object> claims, Class<USER> userClass) {
+        if (claims.get(ClaimKey.SUBJECT).equals(getJimaConfigProperties().getSecurity().getDefaultClientRegistrationId())) {
+            if (Strings.isNotBlank(userInfo)) {
+                return getUser(userInfo, userClass);
+            } else {
+                return Optional.empty();
+            }
+        } else {
+            return getUser(claims);
+        }
+    }
+
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>> Optional<USER> getUser(USER user, Map<String, Object> claims) {
+        if (claims.get(ClaimKey.SUBJECT).equals(getJimaConfigProperties().getSecurity().getDefaultClientRegistrationId())) {
+            if (user != null) {
+                return Optional.of(user);
+            } else {
+                return Optional.empty();
+            }
+        } else {
+            return getUser(claims);
+        }
+    }
+
+    @Override
+    default <ID extends Comparable<ID> & Serializable, USER extends BaseUser<ID>, P extends Principal> Optional<USER> getUser(String userInfo, P principal, Class<USER> userClass) {
+        if (principal.getName().equals(getJimaConfigProperties().getSecurity().getDefaultClientRegistrationId())) {
+            if (Strings.isNotBlank(userInfo)) {
+                return getUser(userInfo, userClass);
+            } else {
+                return Optional.empty();
+            }
+        } else {
+            return getUser(principal);
+        }
+    }
+
+
+}
---
+++ api/restful/oauth2/src/main/java/ir/msob/jima/security/api/restful/oauth2/WebClientConfiguration.java
@@ -0,0 +1,73 @@
+package ir.msob.jima.security.api.restful.oauth2;
+
+import ir.msob.jima.core.beans.configuration.JimaConfigProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * This configuration class, WebClientConfiguration, is responsible for configuring and customizing the behavior of WebClient instances.
+ * It sets up OAuth2 client integration, load balancing, and logging of requests.
+ * <p>
+ * Author: Yaqub Abdi
+ */
+@CommonsLog
+@Configuration
+@RequiredArgsConstructor
+public class WebClientConfiguration {
+
+    private final JimaConfigProperties jimConfigProperties;
+
+    /**
+     * Create an ExchangeFilterFunction for logging HTTP requests.
+     *
+     * @return The ExchangeFilterFunction that logs requests.
+     */
+    private static ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Request: " + clientRequest.method() + " " + clientRequest.url());
+            return Mono.just(clientRequest);
+        });
+    }
+
+    /**
+     * Create a primary WebClient instance for making HTTP requests.
+     *
+     * @param builder The WebClient.Builder used to create the WebClient instance.
+     * @return The primary WebClient instance.
+     */
+    @Bean
+    @Primary
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+
+    /**
+     * Create a primary load-balanced WebClient.Builder with OAuth2 integration.
+     *
+     * @param authorizedClientManager The manager for OAuth2 authorized clients.
+     * @return The load-balanced WebClient.Builder instance.
+     */
+    @Bean
+    @Primary
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+        // Create a ServerOAuth2AuthorizedClientExchangeFilterFunction to handle OAuth2 integration.
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth.setDefaultOAuth2AuthorizedClient(true);
+        oauth.setDefaultClientRegistrationId(jimConfigProperties.getSecurity().getDefaultClientRegistrationId());
+
+        return WebClient.builder()
+                .filter(oauth)  // Apply OAuth2 client integration.
+                .filters(exchangeFilterFunctions -> exchangeFilterFunctions.add(logRequest()));  // Log HTTP requests.
+    }
+}
